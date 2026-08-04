79315e2be813167414773ba16b9ef38cc80e8f49
Deva <ddeva3202@gmail.com>
2026-08-04 10:12:12 +0530

    ci: convert deploy-orderservice to multi-job DAG pipeline for visual graph

--- orderservice/src/main/java/com/deva/orderservice/controller/OrderController.java
+++ orderservice/src/main/java/com/deva/orderservice/controller/OrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// Multi-job CI/CD pipeline trigger test
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
