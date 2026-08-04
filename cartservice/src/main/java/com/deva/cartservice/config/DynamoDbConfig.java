60b1b244693c042d647f588d768ea3201d10f324
Deva <ddeva3202@gmail.com>
2026-08-04 17:12:01 +0530

    feat(xray): add AWS SDK TracingInterceptor to all microservice DynamoDbClient beans

--- cartservice/src/main/java/com/deva/cartservice/config/DynamoDbConfig.java
+++ cartservice/src/main/java/com/deva/cartservice/config/DynamoDbConfig.java
@@ -1,28 +1,27 @@
 package com.deva.cartservice.config;
 
+import com.amazonaws.xray.interceptors.TracingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import com.amazonaws.xray.interceptors.TracingInterceptor;
-import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 
-
-import java.net.URI;
 @Configuration
 public class DynamoDbConfig {
 
-    @Value("${aws.dynamodb.region}")
+    @Value("${aws.dynamodb.region:us-east-1}")
     private String region;
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
                 .region(Region.of(region))
+                .overrideConfiguration(ClientOverrideConfiguration.builder()
+                        .addExecutionInterceptor(new TracingInterceptor())
+                        .build())
                 .build();
     }
 
--- productservice/src/main/java/com/deva/productservice/config/DynamoDbConfig.java
+++ productservice/src/main/java/com/deva/productservice/config/DynamoDbConfig.java
@@ -1,8 +1,10 @@
 package com.deva.productservice.config;
 
+import com.amazonaws.xray.interceptors.TracingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -17,6 +19,9 @@ public class DynamoDbConfig {
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
                 .region(Region.of(region))
+                .overrideConfiguration(ClientOverrideConfiguration.builder()
+                        .addExecutionInterceptor(new TracingInterceptor())
+                        .build())
                 .build();
     }
 
